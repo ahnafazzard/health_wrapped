f6a258ccfa9fc42afd4da0eb2cc09d4e12aa203f
Arjun Bawa <arj1211@users.noreply.github.com>
2024-12-29 20:48:22 -0500

    done data parsing and modelling for health records

--- backend/health_models.py
+++ backend/health_models.py
@@ -3,46 +3,58 @@ from datetime import datetime
 
 
 @dataclass
-class DistanceWalkingRunningRecord:
-    # "unit": "km",
-    unit: str
-    # "creationDate": "2021-10-13 06:52:09 -0500",
+class BaseHealthRecord:
     creationDate: datetime
-    # "startDate": "2021-10-13 06:39:32 -0500",
     startDate: datetime
-    # "endDate": "2021-10-13 06:49:32 -0500",
     endDate: datetime
-    # "value": "0.0804713"
+
+    def __init__(self, record: dict):
+        self.creationDate = datetime.strptime(
+            record.get("creationDate"), "%Y-%m-%d %H:%M:%S %z"
+        )
+        self.startDate = datetime.strptime(
+            record.get("startDate"), "%Y-%m-%d %H:%M:%S %z"
+        )
+        self.endDate = datetime.strptime(record.get("endDate"), "%Y-%m-%d %H:%M:%S %z")
+
+
+@dataclass
+class DistanceWalkingRunningRecord(BaseHealthRecord):
+    unit: str
     value: float
 
     def __init__(self, record: dict):
+        super().__init__(record)
         self.unit = record.get("unit")
-        self.creationDate = datetime.strptime(
-            record.get("creationDate"), "%Y-%m-%d %H:%M:%S %z"
-        )
-        self.startDate = datetime.strptime(
-            record.get("startDate"), "%Y-%m-%d %H:%M:%S %z"
-        )
-        self.endDate = datetime.strptime(record.get("endDate"), "%Y-%m-%d %H:%M:%S %z")
         self.value = float(record.get("value"))
 
 
-class StepCountRecord:
-    # "creationDate": "2021-10-13 06:52:09 -0500",
-    creationDate: datetime
-    # "startDate": "2021-10-13 06:39:32 -0500",
-    startDate: datetime
-    # "endDate": "2021-10-13 06:49:32 -0500",
-    endDate: datetime
-    # "count": "207"
+@dataclass
+class StepCountRecord(BaseHealthRecord):
     count: int
 
     def __init__(self, record: dict):
-        self.creationDate = datetime.strptime(
-            record.get("creationDate"), "%Y-%m-%d %H:%M:%S %z"
-        )
-        self.startDate = datetime.strptime(
-            record.get("startDate"), "%Y-%m-%d %H:%M:%S %z"
-        )
-        self.endDate = datetime.strptime(record.get("endDate"), "%Y-%m-%d %H:%M:%S %z")
+        super().__init__(record)
         self.count = int(record.get("value"))
+
+
+@dataclass
+class VO2MaxRecord(BaseHealthRecord):
+    unit: str
+    value: float
+
+    def __init__(self, record: dict):
+        super().__init__(record)
+        self.unit = record.get("unit")
+        self.value = float(record.get("value"))
+
+
+@dataclass
+class ActiveEnergyBurnedRecord(BaseHealthRecord):
+    unit: str
+    value: float
+
+    def __init__(self, record: dict):
+        super().__init__(record)
+        self.unit = record.get("unit")
+        self.value = float(record.get("value"))
--- backend/health_records.py
+++ backend/health_records.py
@@ -1,6 +1,11 @@
 from typing import Any, Dict
 
-from health_models import DistanceWalkingRunningRecord, StepCountRecord
+from health_models import (
+    ActiveEnergyBurnedRecord,
+    DistanceWalkingRunningRecord,
+    StepCountRecord,
+    VO2MaxRecord,
+)
 
 
 class HealthRecords:
@@ -28,11 +33,137 @@ class HealthRecords:
             )
         )
 
-        # self.VO2Max_records = filter(
-        #     lambda x: x["type"] == "HKQuantityTypeIdentifierVO2Max", self.records
-        # )
+        self.VO2Max_records = list(
+            map(
+                VO2MaxRecord,
+                filter(
+                    lambda x: x["type"] == "HKQuantityTypeIdentifierVO2Max",
+                    self.records,
+                ),
+            )
+        )
 
-        # self.ActiveEnergyBurned_records = filter(
-        #     lambda x: x["type"] == "HKQuantityTypeIdentifierActiveEnergyBurned",
-        #     self.records,
-        # )
+        self.ActiveEnergyBurned_records = list(
+            map(
+                ActiveEnergyBurnedRecord,
+                filter(
+                    lambda x: x["type"] == "HKQuantityTypeIdentifierActiveEnergyBurned",
+                    self.records,
+                ),
+            )
+        )
+
+
+if __name__ == "__main__":
+    health_data = {
+        "Record": [
+            {
+                "type": "HKQuantityTypeIdentifierDistanceWalkingRunning",
+                "sourceName": "Mushfiqur's iPhone",
+                "sourceVersion": "15.0",
+                "device": "<<HKDevice: 0x301b13ca0>, name:iPhone, manufacturer:Apple Inc., model:iPhone, hardware:iPhone12,3, software:15.0, creation date:2021-09-22 17:33:35 +0000>",
+                "unit": "km",
+                "creationDate": "2021-10-13 06:52:09 -0500",
+                "startDate": "2021-10-13 06:39:32 -0500",
+                "endDate": "2021-10-13 06:49:32 -0500",
+                "value": "0.0804713",
+            },
+            {
+                "type": "HKQuantityTypeIdentifierDistanceWalkingRunning",
+                "sourceName": "Mushfiqur's iPhone",
+                "sourceVersion": "15.0",
+                "device": "<<HKDevice: 0x301b13ca0>, name:iPhone, manufacturer:Apple Inc., model:iPhone, hardware:iPhone12,3, software:15.0, creation date:2021-09-22 17:33:35 +0000>",
+                "unit": "km",
+                "creationDate": "2021-10-13 07:02:07 -0500",
+                "startDate": "2021-10-13 06:51:05 -0500",
+                "endDate": "2021-10-13 06:54:18 -0500",
+                "value": "0.0287828",
+            },
+            {
+                "type": "HKQuantityTypeIdentifierStepCount",
+                "sourceName": "Mushfiqur\u2019s Apple\u00a0Watch",
+                "sourceVersion": "8.0",
+                "device": "<<HKDevice: 0x301b11310>, name:Apple Watch, manufacturer:Apple Inc., model:Watch, hardware:Watch6,1, software:8.0, creation date:2021-09-25 06:03:07 +0000>",
+                "unit": "count",
+                "creationDate": "2021-10-14 05:52:39 -0500",
+                "startDate": "2021-10-14 05:40:38 -0500",
+                "endDate": "2021-10-14 05:50:39 -0500",
+                "value": "207",
+            },
+            {
+                "type": "HKQuantityTypeIdentifierStepCount",
+                "sourceName": "Mushfiqur\u2019s Apple\u00a0Watch",
+                "sourceVersion": "8.0",
+                "device": "<<HKDevice: 0x301b11310>, name:Apple Watch, manufacturer:Apple Inc., model:Watch, hardware:Watch6,1, software:8.0, creation date:2021-09-25 06:03:07 +0000>",
+                "unit": "count",
+                "creationDate": "2021-10-14 06:02:36 -0500",
+                "startDate": "2021-10-14 05:52:09 -0500",
+                "endDate": "2021-10-14 06:01:40 -0500",
+                "value": "419",
+            },
+            {
+                "type": "HKQuantityTypeIdentifierVO2Max",
+                "sourceName": "Mushfiqur\u2019s Apple\u00a0Watch",
+                "sourceVersion": "2663.1.1",
+                "unit": "mL/min\u00b7kg",
+                "creationDate": "2021-10-12 16:39:43 -0500",
+                "startDate": "2021-10-12 16:39:43 -0500",
+                "endDate": "2021-10-12 16:39:43 -0500",
+                "value": "38.57",
+                "MetadataEntry": [
+                    {"key": "HKVO2MaxTestType", "value": "2"},
+                    {"key": "HKMetadataKeySyncVersion", "value": "1"},
+                    {
+                        "key": "HKMetadataKeySyncIdentifier",
+                        "value": "1A4C8486-A435-4192-8492-BD41E63BB6FC",
+                    },
+                ],
+            },
+            {
+                "type": "HKQuantityTypeIdentifierVO2Max",
+                "sourceName": "Mushfiqur\u2019s Apple\u00a0Watch",
+                "sourceVersion": "2664.0.8.1.1",
+                "unit": "mL/min\u00b7kg",
+                "creationDate": "2021-10-31 14:23:53 -0500",
+                "startDate": "2021-10-31 14:23:52 -0500",
+                "endDate": "2021-10-31 14:23:52 -0500",
+                "value": "39.14",
+                "MetadataEntry": [
+                    {"key": "HKVO2MaxTestType", "value": "2"},
+                    {"key": "HKMetadataKeySyncVersion", "value": "1"},
+                    {
+                        "key": "HKMetadataKeySyncIdentifier",
+                        "value": "332884B7-202B-410F-9587-837DC7DB32F3",
+                    },
+                ],
+            },
+            {
+                "type": "HKQuantityTypeIdentifierActiveEnergyBurned",
+                "sourceName": "Mushfiqur\u2019s Apple\u00a0Watch",
+                "sourceVersion": "8.0",
+                "device": "<<HKDevice: 0x301b10dc0>, name:Apple Watch, manufacturer:Apple Inc., model:Watch, hardware:Watch6,1, software:8.0, creation date:2021-09-25 06:03:07 +0000>",
+                "unit": "Cal",
+                "creationDate": "2021-10-13 22:23:32 -0500",
+                "startDate": "2021-10-13 22:20:30 -0500",
+                "endDate": "2021-10-13 22:21:31 -0500",
+                "value": "0.45",
+            },
+            {
+                "type": "HKQuantityTypeIdentifierActiveEnergyBurned",
+                "sourceName": "Mushfiqur\u2019s Apple\u00a0Watch",
+                "sourceVersion": "8.0",
+                "device": "<<HKDevice: 0x301b10dc0>, name:Apple Watch, manufacturer:Apple Inc., model:Watch, hardware:Watch6,1, software:8.0, creation date:2021-09-25 06:03:07 +0000>",
+                "unit": "Cal",
+                "creationDate": "2021-10-13 22:23:32 -0500",
+                "startDate": "2021-10-13 22:21:31 -0500",
+                "endDate": "2021-10-13 22:22:33 -0500",
+                "value": "0.414",
+            },
+        ]
+    }
+
+    records = HealthRecords(health_data)
+    print(records.DistanceWalkingRunning_records)
+    print(records.StepCount_records)
+    print(records.VO2Max_records)
+    print(records.ActiveEnergyBurned_records)
